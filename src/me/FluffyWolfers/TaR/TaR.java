e41cf0127c2786140e7e70d83cf183251362b38d
FluffyNarwhals <fluffynarwhalsgaming@gmail.com>
2013-08-08 22:13:52 -0700

    Added ability to control mob while riding it.

--- src/me/FluffyWolfers/TaR/TaR.java
+++ src/me/FluffyWolfers/TaR/TaR.java
@@ -4,7 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TaR extends JavaPlugin{
 	
+	public static TaR t;
+	
 	public void onEnable(){
+		
+		t = this;
 		
 		this.getServer().getPluginManager().registerEvents(new TaRListener(), this);
 		
--- src/me/FluffyWolfers/TaR/TaRListener.java
+++ src/me/FluffyWolfers/TaR/TaRListener.java
@@ -1,12 +1,20 @@
 package me.FluffyWolfers.TaR;
 
+import net.minecraft.server.v1_6_R2.EntityCreature;
+import net.minecraft.server.v1_6_R2.PathEntity;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_6_R2.entity.CraftCreature;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class TaRListener implements Listener{
 	
@@ -14,20 +22,50 @@ public class TaRListener implements Listener{
 	public void getOnMob(PlayerInteractEntityEvent e){
 		
 		Player p = e.getPlayer();
+		
 		Entity en = e.getRightClicked();
+		LivingEntity l = (LivingEntity) en;
 		
 		if(p.getItemInHand().getType().equals(Material.SADDLE)){
 			
-			if(p.isSneaking()){
+			if(p.hasPermission("takearide.ride")){
 				
-				if(p.hasPermission("takearide.ride")){
-					
-					en.setPassenger(p);
-					
-				}
+				l.setPassenger(p);
 				
 			}
 			
+		}
+			
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void controlMob(PlayerInteractEvent e){
+		
+		Player p = e.getPlayer();
+		
+		if(p.getVehicle() != null){
+
+			Entity en = p.getVehicle();
+			LivingEntity l = (LivingEntity) en;
+			
+			if(p.hasPermission("takearide.control")){
+				
+				//l.setVelocity(p.getEyeLocation().getDirection());
+				
+				Location to = p.getLocation();
+				
+				Block b = p.getTargetBlock(null, 200);
+				
+				to.setX(b.getX());
+				to.setY(b.getY());
+				to.setZ(b.getZ());
+				
+                EntityCreature ec = ((CraftCreature)l).getHandle();
+                PathEntity path = ec.getNavigation().a(to.getX(), to.getY(), to.getZ());
+				ec.getNavigation().a(path, 1.35F);
+                
+			}
+				
 		}
 			
 	}
