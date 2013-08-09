3cb62f5db75abd2275cca0af9afea0143101bae9
FluffyNarwhals <fluffynarwhalsgaming@gmail.com>
2013-08-08 23:25:13 -0700

    Fixed stuff.

--- src/me/FluffyWolfers/TaR/TaR.java
+++ src/me/FluffyWolfers/TaR/TaR.java
@@ -4,17 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TaR extends JavaPlugin{
-	
+
 	public static TaR t;
-	
+
 	public void onEnable(){
-		
+
 		Bukkit.getLogger().info("[TakeARide] Starting up...");
-		
+
 		t = this;
-		
+
 		this.getServer().getPluginManager().registerEvents(new TaRListener(), this);
-		
+
 	}
-	
+
 }
