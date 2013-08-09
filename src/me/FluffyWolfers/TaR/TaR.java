9d7ff8cf5bf01e2aa68593d7ccdb313ef0c3d4f4
FluffyNarwhals <fluffynarwhalsgaming@gmail.com>
2013-08-08 23:33:53 -0700

    Added version.

--- src/me/FluffyWolfers/TaR/TaR.java
+++ src/me/FluffyWolfers/TaR/TaR.java
@@ -1,17 +1,20 @@
 package me.FluffyWolfers.TaR;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TaR extends JavaPlugin{
 
 	public static TaR t;
+	public static PluginDescriptionFile pdf;
 
 	public void onEnable(){
 
-		Bukkit.getLogger().info("[TakeARide] Starting up...");
-
 		t = this;
+		pdf = this.getDescription();
+		
+		Bukkit.getLogger().info("[TakeARide v" + pdf.getVersion() + "] Starting up...");
 
 		this.getServer().getPluginManager().registerEvents(new TaRListener(), this);
 
